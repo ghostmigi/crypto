a7e969c6a177f009999ab31de5858d620ebf4ad7
Karim <saber.karim1991@gmail.com>
2021-10-21 22:02:52 +0100

    first commit

--- src/Components/CoinsTable.js
+++ src/Components/CoinsTable.js
@@ -1,14 +1,88 @@
+import {
+  Container,
+  createTheme,
+  LinearProgress,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CoinList } from "../config/api";
+import { CryptoState } from "../CryptoContext";
 
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const { currency } = CryptoState();
+
   const fetchCoins = async () => {
-    const { data } = await axios.get(CoinList);
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    setCoins(data);
+    setLoading(false);
   };
-  return <div>Coins Table</div>;
+
+  console.log(coins);
+  useEffect(() => {
+    fetchCoins();
+  }, [currency]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography
+          variant="h4"
+          style={{ margin: 18, fontFamily: "Montserrat" }}>
+          Crypto Currency Prices By Market Cap
+        </Typography>
+        <TextField
+          label="Search For a Crypto Currency.."
+          variant="outlined"
+          style={{ matginBottom: 20, width: "100%" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <TableContainer>
+          {loading ? (
+            <LinearProgress style={{ backgroundColor: "gold" }} />
+          ) : (
+            <Table>
+              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                <TableRow>
+                  {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Montserrat",
+                      }}
+                      key={head}
+                      align={head === "Coin" ? "" : "right"}>
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+            </Table>
+          )}
+        </TableContainer>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default CoinsTable;
